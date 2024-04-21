ec789282aeddd57752015b255155ba0db01b253c
mehmet fiskindal <mehmetfiskindal@gmail.com>
2024-04-21 15:32:55 +0300

    MosqueSoundControlStrategy

--- lib/features/silence/services/silence.dart
+++ lib/features/silence/services/silence.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/services.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-final class SilenceMode {
-  void changeRingtoneMode() {
-    AudioPlayer player = AudioPlayer();
-    player.setVolume(0);
+abstract class MosqueSoundControlStrategy {
+  void adjustVolume(AudioPlayer audioPlayer);
+}
+
+class InMosqueSoundControlStrategy implements MosqueSoundControlStrategy {
+  @override
+  void adjustVolume(AudioPlayer audioPlayer) {
+    audioPlayer.setVolume(0.0); // Silent in mosque
+  }
+}
+
+class NormalSoundControlStrategy implements MosqueSoundControlStrategy {
+  @override
+  void adjustVolume(AudioPlayer audioPlayer) {
+    audioPlayer.setVolume(100.0); // Normal volume level
+  }
+}
+
+class MosqueSoundControl {
+  MosqueSoundControlStrategy? _strategy;
+
+  MosqueSoundControl(this._strategy);
+
+  void adjustSound(AudioPlayer audioPlayer) {
+    _strategy?.adjustVolume(audioPlayer);
+  }
+
+  void setStrategy(MosqueSoundControlStrategy newStrategy) {
+    _strategy = newStrategy;
   }
 }

d3653e657f61d96b83e38e40787d19ab297487af
mehmet fiskindal <mehmetfiskindal@gmail.com>
2024-06-23 21:36:32 +0300

    chore: Update sound_mode dependency to version 3.0.0

--- lib/features/silence/services/silence.dart
+++ lib/features/silence/services/silence.dart
@@ -1,59 +1,39 @@
-import 'package:flutter/services.dart';
-import 'package:permission_handler/permission_handler.dart'; // Import the permission_handler package
+import 'package:sound_mode/sound_mode.dart';
 import 'package:sound_mode/utils/ringer_mode_statuses.dart';
+
+abstract class MosqueSoundControlStrategy {
+  void adjustVolume(RingerModeStatus status);
+}
+
+class InMosqueSoundControlStrategy implements MosqueSoundControlStrategy {
+  @override
+  void adjustVolume(RingerModeStatus status) {
+    SoundMode.setSoundMode(RingerModeStatus.silent);
+  }
+}
+
+class NormalSoundControlStrategy implements MosqueSoundControlStrategy {
+  @override
+  void adjustVolume(RingerModeStatus status) {
+    SoundMode.setSoundMode(RingerModeStatus.normal);
+  }
+}
 
 class MosqueSoundControl {
   static final MosqueSoundControl _instance = MosqueSoundControl._internal();
-  MosqueSoundControlStrategy? _strategy;
 
   factory MosqueSoundControl() {
     return _instance;
   }
 
   MosqueSoundControl._internal();
+  MosqueSoundControlStrategy? _strategy;
 
-  void setStrategy(MosqueSoundControlStrategy strategy) {
-    _strategy = strategy;
+  void adjustSound(RingerModeStatus status) {
+    _strategy?.adjustVolume(RingerModeStatus.values[status.index]);
   }
 
-  Future<void> adjustSound(RingerModeStatus mode) async {
-    try {
-      if (_strategy != null) {
-        await _strategy!.adjustSound(mode);
-      }
-    } on PlatformException catch (e) {
-      _log('Failed to adjust sound: ${e.message}');
-    }
-  }
-
-  Future<void> checkPermission() async {
-    try {
-      if (await Permission.notification.isDenied) {
-        await Permission.notification.request();
-      }
-      if (await Permission.notification.isGranted) {
-        print('Permission granted.');
-      } else {
-        await Permission.notification.request();
-      }
-    } on PlatformException catch (e) {
-      _log('Failed to check permission: ${e.message}');
-    }
-  }
-
-  void _log(String message) {
-    // Implement a logging framework or method here
-    print(message); // Replace this with a logging framework
-  }
-}
-
-abstract class MosqueSoundControlStrategy {
-  Future<void> adjustSound(RingerModeStatus mode);
-}
-
-class NormalSoundControlStrategy implements MosqueSoundControlStrategy {
-  @override
-  Future<void> adjustSound(RingerModeStatus mode) async {
-    // Implement the logic for adjusting to normal sound mode
+  void setStrategy(MosqueSoundControlStrategy newStrategy) {
+    _strategy = newStrategy;
   }
 }
--- lib/features/silence/view/silence_mixin.dart
+++ lib/features/silence/view/silence_mixin.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:quietmasjid/features/silence/view/silence.dart';
 import 'package:quietmasjid/features/silence/services/silence.dart';
+import 'package:sound_mode/permission_handler.dart';
+import 'package:sound_mode/sound_mode.dart';
 import 'package:sound_mode/utils/ringer_mode_statuses.dart';
-import 'package:flutter/services.dart';
 
 mixin SilenceViewMixin on State<SilenceView> {
   MosqueSoundControlStrategy? _strategy;
@@ -11,30 +13,23 @@ mixin SilenceViewMixin on State<SilenceView> {
   void initState() {
     super.initState();
     _strategy = NormalSoundControlStrategy(); // Initial strategy
-    permissionCheckRequest(); // Check and request permission if necessary
   }
 
   void permissionCheckRequest() async {
-    try {
-      await MosqueSoundControl().checkPermission();
-    } on PlatformException catch (e) {
-      // Handle exception
-      print('Failed to check permission: ${e.message}');
+    bool? isGranted = await PermissionHandler.permissionsGranted;
+
+    if (isGranted != null && !isGranted) {
+      // Opens the Do Not Disturb Access settings to grant the access
+      await PermissionHandler.openDoNotDisturbSetting();
     }
   }
 
   Future<void> setSilenceMode(MosqueSoundControlStrategy strategy) async {
-    try {
-      setState(() {
-        _strategy = strategy;
-        MosqueSoundControl().setStrategy(strategy); // Update singleton strategy
-      });
-
-      await MosqueSoundControl().adjustSound(RingerModeStatus.silent);
-    } on PlatformException catch (e) {
-      // Handle exception
-      print('Failed to set silence mode: ${e.message}');
-    }
+    setState(() {
+      _strategy = strategy;
+      MosqueSoundControl().setStrategy(strategy); // Update singleton strategy
+      MosqueSoundControl().adjustSound(RingerModeStatus.silent);
+    });
   }
 
   // You can optionally expose a getter for the current strategy
